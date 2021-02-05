5accfc3f881a7f3a1ee8d876aa5770bcc567eb1d
Felipe <felipeslopes2010@hotmail.com>
2021-02-05 17:38:15 -0300

    Estudo

---
+++ sintaxe-variaveis-e-fluxo/src/TestaEscopo.java
@@ -0,0 +1,22 @@
+
+public class TestaEscopo {
+
+	public static void main(String[] args) {
+		System.out.println("Testando condicionais");
+
+		int idade = 20;
+		int quantidadePessoas = 3;
+
+		// boolean acompanhado = quantidaedPessoas >= 2;
+
+		boolean acompanhado;
+
+		if (quantidadePessoas >= 2) {
+			acompanhado = true;
+		} else {
+			acompanhado = false;
+		}
+		System.out.println("Valor de acompanhado = " + acompanhado);
+	}
+
+}
