d4872e4d7cc3294f4ebfbe584caa23aeaf0111e7
Felipe <felipeslopes2010@hotmail.com>
2021-02-08 17:40:36 -0300

    Java Polimorfismo: Entenda herança e interfaces

---
+++ bytebank-herdado/src/EditorVideo.java
@@ -0,0 +1,9 @@
+//Gerente é um funcionário
+public class EditorVideo extends Funcionario{
+
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificação do EDITOR DE VÍDEO");
+		return super.getBonificacao() + 100;
+	}
+
+}
