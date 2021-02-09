e167b9cbec1e835b50c2de8f89fedb9e69cc895b
Felipe <felipeslopes2010@hotmail.com>
2021-02-09 16:41:48 -0300

    Java Polimorfismo: Entenda herança e interfaces

--- bytebank-herdado/src/Funcionario.java
+++ bytebank-herdado/src/Funcionario.java
@@ -1,32 +1,48 @@
 
-public class Funcionario {
+public abstract class Funcionario {
 
 	private String nome;
 	private String cpf;
 	private double salario;
-	
-	public double getBonificacao() {
-		return this.salario * 0.05;
+	private int senha;
+
+	public void setSenha(int senha) {
+		this.senha = senha;
 	}
-	
+
+	public boolean autentica(int senha) {
+		if (this.senha == senha) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	// metodo sem corpo, não há implementação
+	public abstract double getBonificacao();
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getCpf() {
 		return cpf;
 	}
+
 	public void setCpf(String cpf) {
 		this.cpf = cpf;
 	}
+
 	public double getSalario() {
 		return salario;
 	}
+
 	public void setSalario(double salario) {
 		this.salario = salario;
 	}
-	
-	
+
 }
--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,23 +1,25 @@
-//Gerente é um funcionário
-public class Gerente extends Funcionario{
+//Gerente é um Funcionário, Gerente herda da class Funcionário, assina o contrato Autenticavel
 
+public class Gerente extends Funcionario implements Autenticavel {
+	
 	private int senha;
 
+	public double getBonificacao() {
+		System.out.println("Chamando o método de bonificacao do GERENTE");
+		return super.getSalario();
+	}
+
+	@Override
 	public void setSenha(int senha) {
 		this.senha = senha;
 	}
-	
+
+	@Override
 	public boolean autentica(int senha) {
-		if (this.senha == senha) {
+		if(this.senha == senha) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-
-	public double getBonificacao() {
-		System.out.println("Chamando o método de bonificação do GERENTE");
-		return super.getBonificacao() + super.getSalario();
-	}
-
 }
